88fea27701ace9c9dd1242597c6cd78a11c60939
Music <gintonic13.6.music@gmail.com>
2024-08-24 10:49:15 +0700

    .

--- n/u/l/l/js/imgOnError.js
+++ n/u/l/l/js/imgOnError.js
@@ -1,21 +1,26 @@
 ﻿function reloadImage(img) {
-    
-    let request;
-    let baseUrl = img.src;
-    if(window.XMLHttpRequest)
-        request = new XMLHttpRequest();
-    else
-        request = new ActiveXObject("Microsoft.XMLHTTP");
-    for (let index = 2; index <= 4; index++) {
-        let checkURl = baseUrl.replace("https://null-library","https://null-library"+index);
-        request?.open('GET', checkURl, false);
-        request?.send(); 
-        if (request.status === 200) {
-            img.onerror = null  ;
-            let url = new URL(checkURl);
-            url.searchParams.set('reload', 'true');
-            img.src = url.toString();
-            break;
+    let baseUrl = "";
+    let arr = img?.src?.split("/n/u/l/l/");
+    if (arr?.length > 0) {
+        baseUrl = arr[arr?.length - 1];
+        if (baseUrl?.length > 0) {
+            let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+            let arr = ['','2','3','4'];
+            for (let index = 0; index < arr?.length; index++) {
+                let host = arr[index];
+                let checkURl = "https://null-library"+host+".github.io/n/u/l/l/";
+                request?.open('GET', checkURl, false);
+                request?.send(); 
+                if (request.status === 200) {
+                    let url = new URL(checkURl);
+                    url.searchParams.set('reload', 'true');
+                    img.src = url.toString();
+                    break;
+
+                }
+            }
         }
-    }
-  }
+    }    
+    img.onerror = null;
+
+}
